74a5e716a04a42a39e4caeb3f767d9db5c14fc90
Justin <richards.justin@gmail.com>
2023-01-11 16:59:54 -0500

    remove hardcoded packge folders

--- tm_js/dist/src/generated/types/MintPrintingTokensViaTokenArgs.d.ts
+++
@@ -1,5 +0,0 @@
-import * as beet from '@metaplex-foundation/beet';
-export declare type MintPrintingTokensViaTokenArgs = {
-    supply: beet.bignum;
-};
-export declare const mintPrintingTokensViaTokenArgsBeet: beet.BeetArgsStruct<MintPrintingTokensViaTokenArgs>;
